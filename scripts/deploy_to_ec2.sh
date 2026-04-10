eee68a73e49bd7ea9dd4bd6d4c235ffe79ef491f
srinirajuk20 <srinirajuk20@gmail.com>
2026-04-10 00:10:15 +0000

    Docker run issue updated

--- scripts/deploy_to_ec2.sh
+++ scripts/deploy_to_ec2.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
+set -euxo pipefail
 
-EC2_IP=$1
-IMAGE_NAME=$2
-TAG=$3
+EC2_IP="$1"
+IMAGE_NAME="$2"
+TAG="$3"
+KEY_PATH="$4"
 
-ssh -o StrictHostKeyChecking=no ubuntu@$EC2_IP << EOF
-
-# Pull latest image
-docker pull $IMAGE_NAME:$TAG
-
-# Stop old container (if exists)
-docker stop flask-app || true
-
-# Remove old container
-docker rm flask-app || true
-
-# Run new container
-docker run -d \
-  --name flask-app \
-    -p 5000:5000 \
-      --restart unless-stopped \
-  $IMAGE_NAME:$TAG
-
-EOF
+ssh -i "$KEY_PATH" -tt -o StrictHostKeyChecking=no "ubuntu@${EC2_IP}" "
+  set -euxo pipefail
+    sudo systemctl start docker
+      sudo docker pull ${IMAGE_NAME}:${TAG}
+        sudo docker stop flask-app || true
+	  sudo docker rm flask-app || true
+	    sudo docker run -d --name flask-app -p 5000:5000 --restart unless-stopped ${IMAGE_NAME}:${TAG}
+	      sudo docker ps -a
+	      "
