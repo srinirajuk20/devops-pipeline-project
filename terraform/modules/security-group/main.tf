249456fce0073f32a510c616087339ee80ee08a4
srinirajuk20 <srinirajuk20@gmail.com>
2026-04-11 23:53:44 +0000

    ssh IP updated

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -15,7 +15,7 @@ resource "aws_security_group" "flask_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["YOUR_PUBLIC_IP/32"]
+    cidr_blocks = ["0.0.0.0/0"]
 }
 
 
