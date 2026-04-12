c90a9905ecf72f4b7af4ab4ca227826941f798e0
srinirajuk20 <srinirajuk20@gmail.com>
2026-04-12 21:20:34 +0000

    Day 15: Add HTTP ingress for Nginx reverse proxy

--- terraform/modules/security-group/main.tf
+++ terraform/modules/security-group/main.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "flask_sg" {
 
   ingress {
     description = "Allow Flask HTTP"
-    from_port   = 5000
-    to_port     = 5000
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
