978b44b43b7d1a8c877ef861c16539b04e51f1bb
srinirajuk20 <srinirajuk20@gmail.com>
2026-04-16 15:15:43 +0000

    Day22 clean image to testing app file updating dynamic

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "DevOps Project Running-Version2"
+    return "DevOps Project Running-Version3 for testing"
 
 @app.route("/health")
 def health():
